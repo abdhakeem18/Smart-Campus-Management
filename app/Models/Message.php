bb414f0158cca56e93ab3caef0bdc99218ab3383
Asmi <shamsudeenasmi96@gmail.com>
2025-03-24 23:45:43 +0530

    middleware web setting home

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -14,7 +14,7 @@ class Message extends Model
     ];
 
     public function message_status(){
-        return $this->hasOne(MessageStatus::class, 'meggase_id','id')->where('user_id', auth()->id());
+        return $this->hasOne(MessageStatus::class, 'message_id','id')->where('user_id', auth()->id());
     }
     public function schedule(){
         return $this->hasOne(Schedule::class, 'id','schedule_id');
